5c184395b99e1f09b236167eeef54df46f0463ec
Megumi <komegi.blog@gmail.com>
2025-04-09 00:11:56 +0900

    Updated to do list function and setup a local storage

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const input = document.querySelector("#todo-input")
+const addBtn = document.querySelector("#add-todo")
+const list = document.querySelector("#todo-list")
+
+//Fetch background image
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
@@ -8,29 +13,31 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         // Use a default background image/author
         document.getElementById("background-filtered").backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
+		// document.getElementById("author").textContent = `By: Dodi Achmad`
     })
 
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => {
-        if (!res.ok) {
-            throw Error("Something went wrong")
-        }
-        return res.json()
-    })
-    .then(data => {
-        document.getElementById("crypto-top").innerHTML = `
-            <img src=${data.image.small} />
-            <span>${data.name}</span>
-        `
-        document.getElementById("crypto").innerHTML += `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>👆: $${data.market_data.high_24h.usd}</p>
-            <p>👇: $${data.market_data.low_24h.usd}</p>
-        `
-    })
-    .catch(err => console.error(err))
+//
+// fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+//     .then(res => {
+//         if (!res.ok) {
+//             throw Error("Something went wrong")
+//         }
+//         return res.json()
+//     })
+//     .then(data => {
+//         document.getElementById("crypto-top").innerHTML = `
+//             <img src=${data.image.small} />
+//             <span>${data.name}</span>
+//         `
+//         document.getElementById("crypto").innerHTML += `
+//             <p>🎯: $${data.market_data.current_price.usd}</p>
+//             <p>👆: $${data.market_data.high_24h.usd}</p>
+//             <p>👇: $${data.market_data.low_24h.usd}</p>
+//         `
+//     })
+//     .catch(err => console.error(err))
 
+//Get the current time
 function getCurrentTime() {
     const date = new Date()
     document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "medium"})
@@ -38,6 +45,7 @@ function getCurrentTime() {
 
 setInterval(getCurrentTime, 1000)
 
+//Get the current location for weather
 navigator.geolocation.getCurrentPosition(position => {
     fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
         .then(res => {
@@ -58,6 +66,7 @@ navigator.geolocation.getCurrentPosition(position => {
         .catch(err => console.error(err))
 });
 
+//Get the current news
 fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${NEWS_API}`)
   .then(res => res.json())
   .then(data =>{
@@ -76,8 +85,7 @@ fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=
 
   })
 
-// const options = {method: 'GET', headers: {accept: 'application/json'}};
-
+//Get the current exchange rate
 fetch(`https://api.exchangerate.host/live?access_key=${EXCHANGE_API}&currencies=USD,JPY`)
   .then(res => res.json())
   .then(data => {
@@ -87,3 +95,51 @@ fetch(`https://api.exchangerate.host/live?access_key=${EXCHANGE_API}&currencies=
     `
 
   })
+
+//Add a to do list
+
+//get the list from local storage
+let todos = JSON.parse(localStorage.getItem("todos")) || []
+renderTodos()
+
+addBtn.addEventListener("click", function(){
+    if (input.value) {
+        todos.push(input.value)
+        input.value = ""
+        localStorage.setItem("todos", JSON.stringify(todos))
+        renderTodos()
+    }
+})
+
+function renderTodos(){
+    list.innerHTML = todos.map((todo,index) => {
+        return `
+            <li>
+                <input type="checkbox" class="todo-checkbox" data-index="${index}" />
+                <span>${todo}</span>
+                <button class="delete-btn">❌</button>
+            </li>
+        `
+    }).join("")
+
+    const deleteBtns = document.querySelectorAll(".delete-btn")
+    deleteBtns.forEach((btn, index) => {
+        btn.addEventListener("click", function(){
+            todos.splice(index, 1)
+            localStorage.setItem("todos", JSON.stringify(todos))
+            renderTodos()
+        })
+    })
+}
+
+const checkboxes = document.querySelectorAll(".todo-checkbox")
+checkboxes.forEach((checkbox, index) => {
+    checkbox.addEventListener("change", function () {
+        const span = this.nextElementSibling
+        if (this.checked) {
+            span.classList.add("done")
+        } else {
+            span.classList.remove("done")
+        }
+    })
+})
